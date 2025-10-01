181a46cbe34cc88e11ebc68c1fb4067e38e74e07
Luke Ponga <developmentdesignsltd@gmail.com>
2025-10-01 10:31:16 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/dashboard/application-form.tsx
+++ src/components/dashboard/application-form.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, 'useEffect' from 'react';
+import React, { useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { CalendarIcon } from 'lucide-react';
